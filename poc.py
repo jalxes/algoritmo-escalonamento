b3932f0daad1636bdbda2bef052ccf512abfb944
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-08-21 10:14:23 -0300

    >

---
+++ poc.py
@@ -0,0 +1,36 @@
+list: [
+    {
+        label: "2",
+        size: "2",
+    }
+    {
+    label: "1",
+    size: "1",
+    }
+    {
+        label: "3",
+        size: "3",
+    }
+]
+
+def FCFS():
+    for value in list:
+        self.run(value)
+def SJNP():
+    for value in list:
+        if minor is not
+        self.run(value)
+def SJP():
+    for value in list:
+        self.run(value)
+def RR():
+    for value in list:
+        self.run(value)
+
+def run(value):
+    pass
+def main():
+
+
+
+    
