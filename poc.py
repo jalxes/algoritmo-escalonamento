fd6cf7e12c2c25eb3f5d6c52652b206db3d203f1
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-08-25 08:22:34 -0300

    quase lá, mentirua falta coisa pra caceta

--- poc.py
+++ poc.py
@@ -1,59 +1,77 @@
+class Process:
+    def __init__(self, start, label, size):
+        self.start = start
+        self.label = label
+        self.size = size
+
 list: [
     {
-        label: "2",
-        size: "2",
-    }
+        "startAt": "2",
+        "label": "2",
+        "size": "2",
+    },
     {
-    label: "1",
-    size: "1",
-    }
+        "startAt": "1",
+        "label": "1",
+        "size": "1",
+    },
     {
-        label: "3",
-        size: "3",
+        "startAt": "3",
+        "label": "3",
+        "size": "3",
     }
+    def list():
+        pass
 ]
+execList: []
 
 def FCFS():
-    actualList = list.first
+    actualList = self.first(list)
     volta = 0;
     while list:
-        volta++
-        print "volta " + volta
+        volta += 1
+        print ("volta " + volta)
         self.run(actualList)
 
 def SJNP():
     # TODO: implementar tempo de chegada
-    actualList = list.first
+    actualList = self.first(list)
     volta = 0;
     while list:
-        volta++
-        print "volta " + volta
+        volta += 1
+        print( "volta " + volta)
         self.run(actualList)
 
 def SJP():
-    actualList = list.first
+    actualList = self.first(list)
     volta = 0;
     while list:
-        volta++
-        print "volta " + volta
+        volta += 1
+        print( "volta " + volta)
         self.run(actualList)
 
 def RR():
-    actualList = list.first
+    actualList = self.first(list)
     counter = 0;
     input
     while list:
-        volta++
-        couter++
+        volta += 1
+        couter += 1
         if couter >= input:
-            actualList = list.next;
-        print "volta " + volta
+            actualList = list.next
+            couter = 0
+        print( "volta " + volta)
         self.run(actualList)
-
-
+def first(list):
+    pass
 def run(actualList):
     if actualList.size == 0:
         self.list.remove(actualList);
-        actualList = self.list.next()
-    actualList.size--
+        actualList = self.next(list)
+    actualList.size-=1
 def main():
+    FCFS()
+    SJNP()
+    SJP()
+    RR()
+main()
