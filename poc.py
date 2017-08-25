d251c11fc4926b18b25f50c285757214e45fd6c7
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-08-25 18:31:05 -0300

    this shit

--- poc.py
+++ poc.py
@@ -4,68 +4,70 @@ class Process:
         self.label = label
         self.size = size
 
-if __name__ == "__main__":
-    x1 = Process(1,1,1)
-    x2 = Process(2,2,2)
-    x3 = Process(3,3,3)
 
-li = [x1, x2, x3]
+li = [Process(num,num,num) for num in range(3)]
 execList = []
-currentLi = [x1, x2, x3]
+currentLi = []
+
 def FCFS():
     currentLi = first(li)
     volta = 0;
     while True:
-        if not len(li):
+        load(volta)
+        if not len(exec):
             break
         volta += 1
         print("volta ", volta)
         run(currentLi)
-
-def SJNP():
-    # TODO: implementar tempo de chegada
-    currentLi = first(li)
-    volta = 0;
-    while li:
-        volta += 1
-        print("volta ", volta)
-        run(currentLi)
-
-def SJP():
-    currentLi = first(li)
-    volta = 0;
-    while li:
-        volta += 1
-        print("volta ", volta)
-        run(currentLi)
-
-def RR():
-    currentLi = first(li)
-    counter = 0;
-    input
-    while li:
-        volta += 1
-        couter += 1
-        if couter >= input:
-            currentLi = next(li)
-            couter = 0
-        print("volta ", volta)
-        run(currentLi)
-
-def first(li):
-    return x1
-def next(li):
-    return x1
-
-def run(item):
-    if not item.size == 0:
-        li.remove(item);
-        item = next(li)
-    item.size-=1
-
+#
+# def SJNP():
+#     # TODO: implementar tempo de chegada
+#     currentLi = first(li)
+#     volta = 0;
+#     while li:
+#         volta += 1
+#         print("volta ", volta)
+#         run(currentLi)
+#
+# def SJP():
+#     currentLi = first(li)
+#     volta = 0;
+#     while li:
+#         volta += 1
+#         print("volta ", volta)
+#         run(currentLi)
+#
+# def RR():
+#     currentLi = first(li)
+#     counter = 0;
+#     input
+#     while li:
+#         volta += 1
+#         couter += 1
+#         if couter >= input:
+#             currentLi = next(li)
+#             couter = 0
+#         print("volta ", volta)
+#         run(currentLi)
+#
+# def first(li):
+#     return x1
+# def next(li):
+#     return x1
+#
+# def load(i):
+#     if li[i]:
+#         execLi.append(li[i])
+#
+# def run(item):
+#     if not item.size == 0:
+#         li.remove(item);
+#         item = next(li)
+#     item.size-=1
+#
 def main():
     FCFS()
-    SJNP()
-    SJP()
-    RR()
+    # SJNP()
+    # SJP()
+    # RR()
 main()
