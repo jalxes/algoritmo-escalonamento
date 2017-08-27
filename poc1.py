97080776e7917747d1479599b6cf74b5f6e1494b
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-08-27 16:22:52 -0300

    WORKS!!!!!!!

--- poc1.py
+++ poc1.py
@@ -4,30 +4,41 @@ class Process:
         self.label = label
         self.size = size
         self.available = True
-li = [Process(num,num,num*2) for num in range(3)]
+
+
+def load(i):
+    if len(li) <= i:
+        return
+    for value in li:
+        if value.start == i & value.available:
+            execLi.append(li[i])
+            li[i].available = False
+
+
+li = [Process(num, num, num + 1 * 2) for num in range(3)]
 execLi = []
-volta = 0;
-execLi.append(li[0])
-atual = execLi[0]
+volta = 0
 totalTime = len(li)
+print(len(li))
+
 while True:
     if totalTime <= 0:
         break
-    for value in li:
-        if value.start == volta & value.available:
-            execLi.append(value)
-            value.available = False
-    if len(execLi):
+    load(volta)
+    if not 'atual' in vars():
+        atual = execLi[volta]
+    print("volta ", volta, ", li ", atual.label, atual.size)
+    if atual:
         atual.size -= 1
         if atual.size <= 0:
+            print("cut the shit out , li ", atual.label, atual.size)
             totalTime -= 1
             execLi.remove(atual)
-            atual = execLi[0]
+            if len(execLi):
+                atual = execLi[0]
     # print("totalTime ", totalTime)
     # print("volta ", volta)
     # print("atual ", atual.label)
     # print("size ", atual.size)
     # print("------")
     volta += 1
-    for value in execLi:
-        print(value.label)
